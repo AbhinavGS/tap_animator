82a116d0279da80f0b129e6d7dcf45fc007199ba
Abhinav Sorate <abhinavsorate01@gmil.com>
2024-01-22 11:56:18 +0530

    add functionality code

--- lib/tap_animator.dart
+++ lib/tap_animator.dart
@@ -1,7 +1,51 @@
 library tap_animator;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'package:flutter/material.dart';
+
+class TapAnimator extends StatefulWidget {
+  final Widget child;
+  final Duration duration;
+  const TapAnimator({
+    super.key,
+    required this.child,
+    this.duration = const Duration(milliseconds: 150),
+  });
+
+  @override
+  State<TapAnimator> createState() => _TapAnimatorState();
+}
+
+class _TapAnimatorState extends State<TapAnimator>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+
+  @override
+  void initState() {
+    _controller = AnimationController(
+      duration: widget.duration,
+      vsync: this,
+    );
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ScaleTransition(
+      scale: Tween<double>(
+        begin: 1.0,
+        end: 0.95,
+      ).animate(_controller),
+      child: Listener(
+        onPointerDown: (_) => _controller.forward(),
+        onPointerUp: (_) => _controller.reverse(),
+        child: widget.child,
+      ),
+    );
+  }
 }
